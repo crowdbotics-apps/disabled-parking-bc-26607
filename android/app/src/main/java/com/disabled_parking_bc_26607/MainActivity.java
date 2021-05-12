d6526a0e649d97ffde7b4fab21eb02f475444d32
Crowdbotics <team@crowdbotics.com>
2021-05-12 15:33:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/disabled_parking_bc_26607/MainActivity.java
@@ -0,0 +1,15 @@
+package com.disabled_parking_bc_26607;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "disabled_parking_bc_26607";
+  }
+}
